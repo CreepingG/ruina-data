7dd68dc45a4347d627ddc62ab5d145e4c2a7cba5
东风有意 <791141513@qq.com>
2021-10-25 19:11:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue'
+import SkillVue from './components/Skill.vue'
+import { Location } from './components/Utils';
+const route = ref(window.location.pathname);
+function handleClick() {
+  Location.set({path:route.value}, false);
+}
+const filterText = ref('');
+</script>
+
+<template>
+  <el-container style="height:100%;width: 100%;">
+    <el-tabs v-model="route" @tab-click="handleClick" style="width: 100%;">
+      <el-tab-pane label="角色" name="/actor">User</el-tab-pane>
+      <el-tab-pane label="技能" name="/skill"><SkillVue :filter="filterText"/></el-tab-pane>
+      <el-tab-pane label="物品" name="/item">Role</el-tab-pane>
+      <el-tab-pane label="敌人" name="/enemy">Task</el-tab-pane>
+    </el-tabs>
+    <el-input v-model="filterText" placeholder="search" size="small"
+      style="position: absolute; left: 0; top: 0; width: 150px; height: 30px; z-index: 9999; padding: 3px;"/>
+  </el-container>
+</template>
+
+<style lang="postcss">
+html, body, #app{
+  height: 100%;
+}
+body{
+  margin: 0;
+}
+.el-tabs{
+  display: flex;
+  flex-direction: column;
+}
+.el-tab-pane{
+  height: 100%;
+}
+.el-tabs__nav{
+  width: 100%;
+  text-align: center;
+}
+</style>
---
+++ src/components/Data.ts
@@ -0,0 +1,60 @@
+import _skills from '../json/skills.json'
+import _attributes from '../json/attributes.json'
+import _states from '../json/states.json'
+import { DataDecorator, TextDecorator, Location } from './Utils'
+
+export const attributes = _attributes.slice(1);
+export const states = _states.slice(1).filter(v=>v.name && !v.name.startsWith('-') && !(v['@id']>=32 && v['@id']<=36));
+
+export type Skill = {
+    [k in  "description" | "using_message1" | "using_message2"]:string|null;
+} & {
+    [k in  "@id" | "type" | "sp_cost" | "scope" | "physical_rate" | "magical_rate" | "variance" | "power" | "hit"]:number;
+} & {
+    [k in  "affect_hp" | "affect_sp" | "affect_attack" | "affect_defense" | "affect_spirit" | "affect_agility" | "absorb_damage" | "ignore_defense" | "affect_attr_defence"]:boolean;
+} & {
+    [k in "state_effects" | "attribute_effects"]: boolean[]
+} & {
+    [k in string]: any
+}
+export const skills = DataDecorator(_skills as unknown as Skill[]);
+
+export const text = {
+    sep: '、',
+    description: '描述',
+    using_message: '使用文本',
+    power: '效果量',
+    magic: '魔力',
+    attack: '攻击力',
+    attribute: '属性',
+    state: '状态',
+    variance: '数值分散度',
+    mp: 'mp消耗',
+    actor: '角色',
+    hit: '成功率',
+    none: '无',
+    effect: TextDecorator({
+      title: '效果',
+      list: ['HP','MP','攻击','防御','魔力','速度']
+    }),
+    tags: TextDecorator({
+      title: '标签',
+      list: ['吸血','无视防御', '属性防御']
+    }),
+    valueDirection: TextDecorator({
+      title: '方向',
+      list: ['提高','降低']
+    }),
+    type: TextDecorator({
+      title: '种类',
+      list: ['一般','传送','逃走','开关']
+    }),
+    scope: TextDecorator({
+      title: '效果范围',
+      list: ['敌方个体','敌方全体','使用者','我方个体','我方全体']
+    }),
+    occasion: TextDecorator({
+      title: '使用场合',
+      list: ['战斗外', '战斗中']
+    }),
+};
